91e90cfa29f9065edcf5fb7fd72bc04657b7ab28
Darren Burgess <darrentburgess@gmail.com>
2014-05-08 09:38:04 -0400

    adding items is now functioning

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@
 $(document).ready(function() {
 
 /*
-General Functions
+Cache 
+	Always try to cache jQuery call at the top so that they 
+	don't get executed more than once
+*/
+
+var $iconCheckboxUnChecked = $('.icon.check-box.unchecked');
+var $iconCheckboxChecked = $('.icon.check-box.checked');
+
+$('input').focus();
+
+/*
+General Functions and plugins
 */
 
 var toggleDisplay = function(className){
@@ -10,40 +21,90 @@ var toggleDisplay = function(className){
 };
 
 $.fn.toggleSprite = function (firstPosition, secondPosition) {
-    return $(this).css('background-position',    function(idx, sp){
+    return $(this).css('background-position',function(idx, sp){
         return sp = sp == firstPosition ? secondPosition : firstPosition;
     });
 };
 
-
-/* 
+/* DONE
 On mouseup view toggle:
 	show and hide completed list
 	toggle position of sprite for eye icon.
 */
-$('.toggle-completed').mouseup(function(){
-	$(this).toggleSprite('-82px 50%', '-101px 50%');
+$('.toggle-completed').bind('click',function(e){
+	$(this).toggleSprite('-82px 50%','-101px 50%');
 	toggleDisplay('.completed-item');
 });
+
+/* 
+On press enter for task item
+	create a new LI in uncomplete list - top of list
+	animate - expand slightly in new position
+	increment the uncompleted count
+	clear the form
+	return to the form field
+*/
+
+$('#input-submit').keyup(function(e) {
+	if(e.which == 13){
+		$('#input-submit').click();
+	}
+});
+
+var itemHtmlFront = '<div class="icon check-box unchecked"></div><span class="item-text">';
+var itemHtmlBack = '</span><div class="icon trash-can"></div><div class="icon drag-grab"></div>';
+
+function addItem() {
+	var newItem = $('#input-field').val().trim();
+	if (newItem){
+			$("<li></li>").appendTo('.uncompleted-item')
+			.html(itemHtmlFront + newItem + itemHtmlBack);
+		}
+	console.log(itemHtmlFront + newItem + itemHtmlBack);
+	$('#input-field').val('');
+}
+
+$('#input-submit').click(addItem);
+
+// $('#input-field').keyup(function(e) {
+// 	// user releases return
+// 	e.preventDefault();
+// 	if(e.keyCode == 13) {
+// 		var newItem = $(this).val().trim();
+// 		alert(newItem);
+// 		if (newItem) {
+// 			console.log(newItem);
+// 		} else {
+// 			console.log("else happened");
+// 		}
+// 	}
+// });
 
 
 /* 
 On mouseup item checkbox (unchecked):
-	move position of sprite to check
-	remove item from uncompleted list(traverse up to li)
-	add item to completed list on bottom
+	•move position of sprite to check
+	•remove item from uncompleted list(traverse up to li)
+	•add item to completed list on bottom
+	addClass() and removeClass()
+	animate the move
 	decrement uncompleted count	
 	increment completed count
 */
-$('.icon.check-box.unchecked').click(function(){
+$('.icon.check-box.unchecked').bind('click',function(e){
 	$(this).toggleSprite('-1px 50%','-21px 50%');
+	console.log(this);
 	$('.completed-item').prepend($(this).closest('li'));
+	$(this).addClass('checked');
+	$(this).removeClass('unchecked');
 });
 
-//This doesn't work for items that have already moved. they still have previous class
-$('.icon.check-box.checked').click(function(){
+$('.icon.check-box.checked').bind('click',function(e){
+	console.log('check query ran');
 	$(this).toggleSprite('-1px 50%','-21px 50%');
 	$('.uncompleted-item').append($(this).closest('li'));
+	$(this).addClass('unchecked');
+	$(this).removeClass('checked');
 });
 
 
@@ -68,6 +129,7 @@ On mouseup sort:
 /* 
 On mouseup trash icon:
 	remove the list item from the UL
+	create a pop-over to confirm
 */
 
 /* 
