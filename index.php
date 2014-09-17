d4e878ef9a4ea2e686ac8d7a2ad735d79226ed3a
Darren Burgess <darrentburgess@gmail.com>
2014-09-17 09:07:05 -0400

    both lists are using FM data

--- index.php
+++ index.php
@@ -1,101 +1,92 @@
 <?php
-    require_once('php/global.php');
-    require_once('php/functions.php');
+	require_once('php/global.php');
+	require_once('php/functions.php');
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/main.css">
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
 
-        <!-- Add your site or application content here -->
+		<!-- Add your site or application content here -->
 
-        <div id="page-wrap">   
+		<div id="page-wrap">   
 
 
-            <img id="background-image" src="img/wood-panel.jpg" alt="" />
+			<img id="background-image" src="img/wood-panel.jpg" alt="" />
 
-            <header>
-                <h1>YATDLA</h1>            
-            </header>
-                <div id="content-wrap">
-                    <form action="#" method="">
-                        <input id="input-field"  type="text" placeholder="Enter a task and press return..." name="Enter a task and press return...">
-                    </form>
+			<header>
+				<h1>YATDLA</h1>            
+			</header>
+				<div id="content-wrap">
+					<form action="#" method="">
+						<input id="input-field"  type="text" placeholder="Enter a task and press return..." name="Enter a task and press return...">
+					</form>
 
-                    <?php 
-                        $records = find_uncompleted_items();
-                        $foundCount = count($records);
-                    ?>
+					<?php 
+						$records = find_items('unchecked');
+						$foundCountUnchecked = count($records);
+					?>
 
-                    <section>
-                        <h2><span id="uncompleted"><?php echo $foundCount;?></span> Uncompleted Tasks
-                        <span class="icon alpha-sort"></span></h2>
-                        <ul class="uncompleted-item">
-                            <?php
-                                foreach ($records as $record) {
-                                    $item = $record->getField('item');
-                                    echo '
-                                    <li>
-                                        <div class="icon check-box unchecked"></div>
-                                        <span class="item-text">'.$item.'</span>
-                                        <div class="icon trash-can"></div>
-                                        <div class="icon drag-grab"></div>
-                                    </li>
-                                    ';
-                                }
-                                
-                            ?>    
-                        </ul>
-                    </section>
+					<section>
+						<h2><span id="uncompleted"><?php echo $foundCountUnchecked;?></span> Uncompleted Tasks
+						<span class="icon alpha-sort"></span></h2>
+						<ul class="uncompleted-item">
+							<?php
+								$list = build_list($records,'unchecked');
+							?>    
+						</ul>
+					</section>
 
-                    <section>
-                        <h2><span id="completed">0</span> Completed Tasks
-                        <span class="icon toggle-completed"></span></h2> 
-                        <ul class="completed-item">
-                            <!-- <li>
-                                <div class="icon check-box checked"></div>
-                                <span class="item-text">Cereal</span>
-                                <div class="icon trash-can"></div>
-                                <div class="icon drag-grab"></div>
-                            </li> -->
-                        </ul>
-                    </section>
+					<?php 
+						$records = find_items('checked');
+						$foundCountUnchecked = count($records);
+					?>
 
-                </div>
-        
-        </div>
+					<section>
+						<h2><span id="completed">0</span> Completed Tasks
+						<span class="icon toggle-completed"></span></h2> 
+						<ul class="completed-item">
+							<?php
+								$list = build_list($records,'checked');
+							?> 
+						</ul>
+					</section>
 
-        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-        <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+				</div>
+		
+		</div>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
-        </script>
-    </body>
+		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+
+		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<script>
+			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+			e.src='//www.google-analytics.com/analytics.js';
+			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+			ga('create','UA-XXXXX-X');ga('send','pageview');
+		</script>
+	</body>
 </html>
--- php/functions.php
+++ php/functions.php
@@ -1,13 +1,13 @@
 <?php
 
-	function testing() {
-		echo "testing123";
-	}
-
-	function find_uncompleted_items() {
+	function find_items($type) {
 		$fm = new FileMaker('yatdla','192.168.1.52','cwp','cwp123');
 		$request = $fm->newFindCommand('web_find_item');
-		$request->addFindCriterion('status', '=');
+		if($type==='unchecked'){
+			$request->addFindCriterion('status', '=');
+		} else {
+			$request->addFindCriterion('status', 1 );
+		}
 		$result = $request->execute();
 		$records = $result->getRecords();
 		if (fm_error(__FUNCTION__,$result) === TRUE) {
@@ -16,6 +16,20 @@
 			return $records;
 		}
 	}
+
+	function build_list($records,$checked) {
+		foreach ($records as $record) {
+            $item = $record->getField('item');
+            echo <<<EOT
+            <li>
+                <div class="icon check-box $checked"></div>
+                <span class="item-text">$item</span>
+                <div class="icon trash-can"></div>
+                <div class="icon drag-grab"></div>
+            </li>
+EOT;
+			}
+        }
 
 	function fm_error($function,$result) {
 		if (FileMaker::isError($result)) {
