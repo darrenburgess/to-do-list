24c16a549f0a4dc9562c9a0bbeda3b040cbfe45a
Darren Burgess <darrentburgess@gmail.com>
2014-09-19 09:25:15 -0400

    added deleteItem.php file

---
+++ php/deleteItem.php
@@ -0,0 +1,11 @@
+<?php
+
+	require_once('global.php');
+	
+	$recId = $_POST['recId'];
+
+	$deleteRequest = $fm->newDeleteCommand('web_find_item', $recId);
+
+	$result = $deleteRequest->execute();
+
+?>
