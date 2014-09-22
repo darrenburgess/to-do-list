dab7434d9dbbe4ab2396e54bbc643eeb2762ee0e
Darren Burgess <darrentburgess@gmail.com>
2014-09-22 00:03:08 -0400

    added toggleStatus.php

---
+++ php/toggleStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+	require_once('global.php');
+	
+	$recId = $_POST['recId'];
+
+	$status = $_POST['status'];
+
+	$editRecord = $fm->newEditCommand('web_find_item', $recId);
+
+	$editRecord->setField('status', $status);
+
+	$result = $editRecord->execute();
+
+
+?>
