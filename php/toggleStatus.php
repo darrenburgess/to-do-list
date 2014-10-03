a66db901f62d88160def55639686fae9216762f1
Darren Burgess <darrentburgess@gmail.com>
2014-10-03 09:34:14 -0400

    toggleStatus function updated to include SQL

--- php/toggleStatus.php
+++ php/toggleStatus.php
@@ -2,9 +2,14 @@
 	require_once('global.php');
 	$recId = $_POST['recId'];
 	$status = $_POST['status'];
-	$editRecord = $fm->newEditCommand('web_find_item', $recId);
-	$editRecord->setField('status', $status);
-	$result = $editRecord->execute();
-	$error = $result->getCode();
-	echo $error;
+	if ($dataBaseType == 'FM') {
+		$editRecord = $fm->newEditCommand('web_find_item', $recId);
+		$editRecord->setField('status', $status);
+		$result = $editRecord->execute();
+		$error = $result->getCode();
+		echo $error;
+	} else { 
+		mysqli_query($connect,'UPDATE item SET status = '.$status.' WHERE id = '.$recId);
+		mysqli_close($connect);
+	}
 ?>
