56ec8e9412d221f12fec3e65071c5d255bd67b62
Darren Burgess <darrentburgess@gmail.com>
2014-10-04 21:01:22 -0400

    added delete function for mySQL database

--- php/deleteItem.php
+++ php/deleteItem.php
@@ -2,8 +2,15 @@
 	require_once('global.php');
 	$recId = $_POST['recId'];
 	echo $recId;
-	$deleteRequest = $fm->newDeleteCommand('web_find_item', $recId);
-	$result = $deleteRequest->execute();
-	$error = $result->getCode();
+	if ($dataBaseType == 'FM') {
+		$deleteRequest = $fm->newDeleteCommand('web_find_item', $recId);
+		$result = $deleteRequest->execute();
+		$error = $result->getCode();
+	} else {
+		$query = "DELETE FROM item WHERE id = ".$recId;
+		if ($connect->query($query)) {
+			printf("Errormessage: %s\n", $connect->error,$query);
+		}
+	}
 	echo $error;
 ?>
